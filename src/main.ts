5703d63d6907348349f4f6c8e991fcdc76bc6bfd
Matan Carmeli <matan.carmeli7@gmail.com>
2024-03-30 17:39:42 +0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { main } from './appMock/app';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
   await app.listen(3000);
 }
 bootstrap();
